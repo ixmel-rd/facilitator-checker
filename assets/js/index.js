071553f18544c7e398483a411a0d1cd04850c362
kiyotaryoma4989 <67671288+kiyotaryoma4989@users.noreply.github.com>
2023-09-27 09:17:41 +0900

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 
 
 async function callApi() {
-    MEMBERS = ["清田","中野","大崎"]
+    MEMBERS = ["大崎","清田","中野"]
     DAY_OF_WEEK = ["（日）","（月）","（火）","（水）","（木）","（金）","（土）"];
     const res = await fetch("https://holidays-jp.github.io/api/v1/date.json");
     let data = await res.json();
