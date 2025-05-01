5cde57673175fce21b13c2a86c54a974f452743a
ixmel-rd <130649180+ixmel-rd@users.noreply.github.com>
2025-05-01 09:00:43 +0900

    順番修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 
 
 async function callApi() {
-    MEMBERS = ["M.M.","K.K.","M.F.","K.M."]
+    MEMBERS = ["K.K.","M.F.","K.M.","M.M."]
     DAY_OF_WEEK = ["（日）","（月）","（火）","（水）","（木）","（金）","（土）"];
     const res = await fetch("https://holidays-jp.github.io/api/v1/date.json");
     let data = await res.json();
