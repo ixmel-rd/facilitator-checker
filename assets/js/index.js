2dd0f9697050a49addcabad7b6e5baf3539048c3
ixmel-rd <130649180+ixmel-rd@users.noreply.github.com>
2025-05-02 08:56:40 +0900

    日数計算を修正

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,10 @@
-
-
 async function callApi() {
     MEMBERS = ["M.M.","K.M.","K.K.","M.F."]
     DAY_OF_WEEK = ["（日）","（月）","（火）","（水）","（木）","（金）","（土）"];
     const res = await fetch("https://holidays-jp.github.io/api/v1/date.json");
     let data = await res.json();
     const HOLIDAYS =  Object.keys(data);
-    let BaseDate = new Date(2023,9 - 1, 11);
+    let BaseDate = new Date(2025,1 - 1, 1);
     let today = new Date();
 
     let yesterday = new Date();
@@ -26,18 +24,24 @@ async function callApi() {
     }
 
     let pastHolidays = 0;
+    let pastWeekends = 0;
+    let isHoliday = false;
     let currentDate = new Date(BaseDate);
     while (currentDate <= today) {
         if (currentDate.getDay() != 0 && currentDate.getDay() != 6) {
             let currentDateStr = currentDate.getFullYear() + "-" + ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" + ("0" + currentDate.getDate()).slice(-2);
             if (HOLIDAYS.includes(currentDateStr)) {
                 pastHolidays++;
+                if (currentDate >= today) {
+                    isHoliday = true;
+                }
             }
+        } else {
+            pastWeekends++;
         }
         currentDate.setDate(currentDate.getDate() + 1);
     }
 
-    let pastWeekends = Math.floor((Math.floor((today - BaseDate) / 86400000)) / 7) * 2;
     let diff = Math.floor((today - BaseDate) / 86400000) - pastWeekends - pastHolidays;
     let hostTodayIndex = diff % MEMBERS.length;
 
@@ -45,7 +49,7 @@ async function callApi() {
     let hostTomorrowIndex = (hostTodayIndex + 1) % MEMBERS.length;
 
     $("#host-yesterday").text(MEMBERS[hostYesterdayIndex])
-    if (today.getDay() == 0 || today.getDay() == 6) {
+    if (today.getDay() == 0 || today.getDay() == 6 || isHoliday) {
         let holidayMessageElem = $("#host-today").parent();
         holidayMessageElem.empty();
         holidayMessageElem.text("今日はお休みです_(:3 」∠)_");
@@ -60,4 +64,3 @@ async function callApi() {
 };
 
 callApi();
-
